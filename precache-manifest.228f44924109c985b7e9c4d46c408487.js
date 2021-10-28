bc3071af37e6ea7921b86b49c7f45107f9382d0a
ast_hryhola <v.hryhola@astoundcommerce.com>
2021-10-28 22:33:00 +0300

    Updates

---
+++ precache-manifest.228f44924109c985b7e9c4d46c408487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a885945adfbbd862901f5e1b5bbc779b",
+    "url": "/wrong-metronome/index.html"
+  },
+  {
+    "revision": "e5cfc2b382f66f101d6e",
+    "url": "/wrong-metronome/static/css/2.5a7cb3a2.chunk.css"
+  },
+  {
+    "revision": "b273e9738b66ed1de290",
+    "url": "/wrong-metronome/static/css/main.de6fd0b7.chunk.css"
+  },
+  {
+    "revision": "e5cfc2b382f66f101d6e",
+    "url": "/wrong-metronome/static/js/2.825090d6.chunk.js"
+  },
+  {
+    "revision": "4275c1d12d633733c6fec0905104cb9a",
+    "url": "/wrong-metronome/static/js/2.825090d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b273e9738b66ed1de290",
+    "url": "/wrong-metronome/static/js/main.f31889af.chunk.js"
+  },
+  {
+    "revision": "5b97ea37a9df9df4d468",
+    "url": "/wrong-metronome/static/js/runtime-main.cf79ffa0.js"
+  }
+]);
